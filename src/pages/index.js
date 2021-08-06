f9f0c9b426168904492db89bc03c4d66f0fc45b4
bnjmnamyers <bnjmn.myers@gmail.com>
2021-08-06 14:22:46 -0400

    Added new map

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,7 +74,7 @@ const IndexPage = ({ size }) => (
                 <h2>Where I WOD</h2>
                 <iframe
                     className="mcf-location-map"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3222.960601779028!2d-83.50979208435382!3d36.11881758009457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885b8b62a36c1a27%3A0xcb9c983bd79d5403!2s318%20W%20Old%20Andrew%20Johnson%20Hwy%2C%20Jefferson%20City%2C%20TN%2037760!5e0!3m2!1sen!2sus!4v1628273205994!5m2!1sen!2sus"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3222.960601779028!2d-83.50979208435382!3d36.11881758009457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885b8d98cd7ac619%3A0xc1b70c45b4981137!2sMossy%20Creek%20Fitness!5e0!3m2!1sen!2sus!4v1628274044192!5m2!1sen!2sus"
                     width="100%"
                     height="785"
                     loading="lazy"
